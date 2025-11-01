b4b1e09cdec3d46c45c3c20034790f1b00766ef1
Mia Dai <daixinming0101@icloud.com>
2025-11-01 13:00:10 -0500

    updated run beast batch file

--- tests/test_scripts/run_beast_resume.sh
+++ tests/test_scripts/run_beast_resume.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -A bezq-delta-gpu
+#SBATCH -A bfsr-delta-gpu
 #SBATCH -p gpuA40x4,gpuA100x4,gpuA40x4-preempt,gpuA100x4-preempt
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -8,26 +8,28 @@
 #SBATCH --mem=100G
 #SBATCH -t 08:00:00
 #SBATCH -J beast_resume
-#SBATCH -o logs/beast_resume_%j.out
-#SBATCH -e logs/beast_resume_%j.err
+#SBATCH -o /work/nvme/bfsr/runs/beast_train_%j.out
+#SBATCH -e /work/nvme/bfsr/runs/beast_train_%j.err
+
 
 # --- Setup environment ---
 source ~/.bashrc
 module load ffmpeg
 conda activate beast
-cd /u/xdai3/artifact/beast
+cd /u/xdai3/code/beast
 
 # --- Define paths ---
 CONFIG="configs/vit.yaml"
-DATA="/u/xdai3/artifact/beast/tests/test_video/extracted_frames"
-CHECKPOINT="/u/xdai3/artifact/beast/runs/2025-10-23/07-58-57/checkpoints/last.ckpt"
+DATA="/work/hdd/bfsr/xdai3/raw_data/beast/test_video1"
+CHECKPOINT="/work/hdd/bfsr/xdai3/raw_data/beast/test_video1/checkpoints/last.ckpt"
+OUTPUT = "/work/nvme/bfsr/xdai3/runs/beast"
 
 # --- Run BEAST ---
 if [ -f "$CHECKPOINT" ]; then
     echo "Found checkpoint: $CHECKPOINT"
     echo "Resuming BEAST training from checkpoint..."
-    beast train --config "$CONFIG" --data "$DATA" --checkpoint "$CHECKPOINT"
+    beast train --config "$CONFIG" --data "$DATA" --checkpoint "$CHECKPOINT" --output "$OUTPUT"
 else
     echo "No checkpoint found. Starting new training run."
-    beast train --config "$CONFIG" --data "$DATA"
+    beast train --config "$CONFIG" --data "$DATA" --output "$OUTPUT"
 fi
