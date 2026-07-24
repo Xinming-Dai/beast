77e0f4f464d7078e51ed05e8e15b7c3c9ef5df4f
Mia Dai <daixinming0101@icloud.com>
2026-07-23 20:12:54 -0500

    update the encoding code

--- scripts/sable_scripts/encoding_decoding/encoding/step2_run_encoding.sh
+++ scripts/sable_scripts/encoding_decoding/encoding/step2_run_encoding.sh
@@ -6,7 +6,7 @@
 #SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=32G
-#SBATCH -t 0-1:59:00
+#SBATCH -t 0-1:00:00
 #SBATCH -J encoding
 #SBATCH -o /u/xdai3/project3d/SBALE_repo/beast/scripts/sable_scripts/encoding_decoding/encoding/step2_run_encoding_%j.log
 #SBATCH --export=ALL
@@ -21,10 +21,10 @@ cd "$REPO_ROOT"
 # Neural encoding: predicts neural activity from Sable latents (RRR/CNN, via Ray Tune).
 NEURAL_INPUT_DIR=/work/hdd/bfsr/xdai3/IBL_data/synchronized/extracted_frames/neural_data   # root dir of neural (spike) data
 
-EID="${EID:-72cb5550-43b4-4ef0-add5-e4adfdfb5e02}"                                
+EID=781b35fd-e1f0-4d14-b2bb-95b7263082bb
 JOB_ID="20284092"                                         
 LATENT_INPUT_DIR=/work/nvme/bfsr/xdai3/project3d/twoview3d_ckpts/beast_sable/ibl_multisession/$JOB_ID/latents
-LATENT_KIND="${LATENT_KIND:-frame}"                         # frame | dino | combined
+LATENT_KIND="${1:-${LATENT_KIND:-frame}}"                  # frame | dino | combined
 
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] Running neural encoding for eid=$EID with latent_kind=$LATENT_KIND"
 
