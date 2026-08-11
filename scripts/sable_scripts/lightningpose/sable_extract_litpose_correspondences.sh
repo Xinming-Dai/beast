5e6d4f7feddc30486f704c5fb11142fc31fd3742
Mia Dai <daixinming0101@icloud.com>
2026-08-11 15:17:41 -0500

    update lightningpose extract script

--- scripts/sable_scripts/lightningpose/sable_extract_litpose_correspondences.sh
+++ scripts/sable_scripts/lightningpose/sable_extract_litpose_correspondences.sh
@@ -2,7 +2,7 @@
 #SBATCH -A bfsr-delta-cpu
 #SBATCH --job-name="correspondences"
 #SBATCH --partition=cpu
-#SBATCH -c 4
+#SBATCH -c 1
 #SBATCH -t 1:00:00
 #SBATCH --mem=10G
 #SBATCH --export=ALL
@@ -12,9 +12,10 @@ exec 2>&1
 source ~/.bashrc
 
 CONFIG=/u/xdai3/project3d/SBALE_repo/beast/configs/multiview/extraction_pipeline_sable.yaml
-SCRIPT=/u/xdai3/project3d/SBALE_repo/beast/beast/preprocess/sable/precompute_litpose_correspondences.py
+SCRIPT=/u/xdai3/project3d/SBALE_repo/beast/beast/preprocess/sable/sable_extract_litpose_correspondences.py
 
 echo "=== executing ==="
 python "${SCRIPT}" \
-    --config "${CONFIG}"
+    --config "${CONFIG}" \
+    --input-dir /work/hdd/bfsr/xdai3/IBL_data/synchronized/extracted_frames/pretrain \
     --overwrite
